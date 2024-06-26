e3f36d1f586f5eadb8f0648734cc7e69bccb4cd3
Mwesi <mwesi@yahoo.com>
2024-06-26 12:04:02 -0400

    Add files via upload
    Started on TTC calculations, Laser ranges taken in, still need to figure out exactly how ranges vector can be used here.

--- lab2_ws/safety_node/src/safety_node.cpp
+++ lab2_ws/safety_node/src/safety_node.cpp
@@ -101,8 +101,14 @@ private:
         /// TODO: update current speed
         // Extract relative speed (linear velocity x) is only needed as this is straight ahead he
         relative_speed_ = -msg->twist.twist.linear.x;
-        double v_x = msg->twist.twist.linear.x;
-        double v_y = msg->twist.twist.linear.y;
+        //double v_x = msg->twist.twist.linear.x;
+        //double v_y = msg->twist.twist.linear.y;
+
+        if(distance_ < 0.1)
+        {
+            brakenow_= true;
+
+        }
 
         // Log the velocities
         RCLCPP_INFO(this->get_logger(), "Speed - x: %f",relative_speed_);
@@ -111,6 +117,9 @@ private:
     void scan_callback(const sensor_msgs::msg::LaserScan::ConstSharedPtr scan_msg) 
     {
         /// TODO: calculate TTC
+
+        distance_ = scan_msg->sensor_msgs::msg::LaserScan::ranges[0];
+
 
         RCLCPP_INFO(this->get_logger(), "Scan: '%f'", scan_msg);
     }
@@ -125,6 +134,7 @@ private:
     double min_TTC = 100;
     double relative_speed_ = 0.0;
     bool brakenow_;
+    double distance_;
     
 };
 int main(int argc, char ** argv) {
