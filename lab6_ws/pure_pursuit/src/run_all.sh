83e90e2002f8e9476508a52f68646c04b74d23e4
Mwesi <mwesi@yahoo.com>
2024-11-24 09:44:27 -0500

    Update run_all.sh

--- lab6_ws/pure_pursuit/src/run_all.sh
+++ lab6_ws/pure_pursuit/src/run_all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Launch first terminal
-gnome-terminal -- bash -c "run_SLAM.sh; exec bash"
+gnome-terminal -- bash -c "./run_SLAM.sh; exec bash"
 
 # Wait 1 second
 sleep 1
 
 # Launch second terminal
-gnome-terminal -- bash -c "rviz2; exec bash"
+gnome-terminal -- bash -c "source /opt/ros/foxy/setup.bash; source install/setup.bash; rviz2; exec bash"
 
 # Wait 1 second
 sleep 1
