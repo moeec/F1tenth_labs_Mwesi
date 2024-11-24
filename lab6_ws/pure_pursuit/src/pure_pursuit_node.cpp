af784b0d20b72b0c1c175ccf2b972489e79d0968
Mwesi <mwesi@yahoo.com>
2024-11-24 12:01:08 -0500

    Update pure_pursuit_node.cpp

--- lab6_ws/pure_pursuit/src/pure_pursuit_node.cpp
+++ lab6_ws/pure_pursuit/src/pure_pursuit_node.cpp
@@ -1,59 +1,38 @@
-#include <sstream>
-#include <string>
-#include <cmath>
-#include <vector>
-
 #include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/laser_scan.hpp"
 #include "nav_msgs/msg/odometry.hpp"
-#include "ackermann_msgs/msg/ackermann_drive_stamped.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
-#include "visualization_msgs/msg/marker.hpp"
-#include <tf2_ros/transform_broadcaster.h>
-#include <tf2/LinearMath/Matrix3x3.h>
-#include <tf2/LinearMath/Quaternion.h>
-/// CHECK: include needed ROS msg type headers and libraries
-
-using namespace std;
 
 class PurePursuit : public rclcpp::Node
 {
-    // Implement PurePursuit
-    // This is just a template, you are free to implement your own node!
+public:
+    PurePursuit() : Node("pure_pursuit")
+    {
+        // Create a subscription to the Odometry message
+        sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+            "/odom", 1000, std::bind(&PurePursuit::callback, this, std::placeholders::_1));
+
+        // Other initialization code...
+    }
 
 private:
+    // Subscription to Odometry
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_;
 
-public:
-    PurePursuit() : Node("pure_pursuit_node")
+    // Callback function for handling Odometry messages
+    void callback(const nav_msgs::msg::Odometry::SharedPtr msg)
     {
-        // TODO: create ROS subscribers and publishers
-
-        rclcpp::Publisher<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr ackermann_publisher_;
-        rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_pub_;
-
-         sub_ = this->create_subscription<nav_msgs::msg::odometry>
-            ("/odom", 1000, std::bind(&SubscribeAndPublish::callback, this, std::placeholders::_1));
-
+        RCLCPP_INFO(this->get_logger(), "Received odometry data");
+        // Process the Odometry message
     }
 
-/// ROS subscribers and publishers
-    rclcpp::Publisher<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr ackermann_publisher_;
-    rclcpp::Subscription<nav_msgs::msg::odometry>::SharedPtr sub_;
-    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_pub_;
-
-    void pose_callback(const geometry_msgs::msg::PoseStamped::ConstPtr &pose_msg)
+    // Callback for PoseStamped messages
+    void pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr pose_msg)
     {
-        // TODO: find the current waypoint to track using methods mentioned in lecture
-
-        // TODO: transform goal point to vehicle frame of reference
-
-        // TODO: calculate curvature/steering angle
-
-        // TODO: publish drive message, don't forget to limit the steering angle.
+        RCLCPP_INFO(this->get_logger(), "Received pose data");
+        // Process the PoseStamped message
     }
-
-    ~PurePursuit() {}
 };
+
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
