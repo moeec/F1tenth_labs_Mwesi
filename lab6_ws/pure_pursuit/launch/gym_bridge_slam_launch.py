941de88f94e08a20189e26a2909f906e133638bf
Mwesi <mwesi@yahoo.com>
2024-12-04 19:55:35 -0500

    Create gym_bridge_slam_launch.py

---
+++ lab6_ws/pure_pursuit/launch/gym_bridge_slam_launch.py
@@ -0,0 +1,72 @@
+  GNU nano 4.8                                                                                                                                                                                          gym_bridge_launch.py                                                                                                                                                                                                     
+# MIT License
+
+# Copyright (c) 2020 Hongrui Zheng
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# Modified (cut down) for SLAM purposes by Mwesigwa Musisi-Nkambwe Decemeber 2024
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+from ament_index_python.packages import get_package_share_directory
+import os
+import yaml
+
+def generate_launch_description():
+    ld = LaunchDescription()
+    config = os.path.join(
+        get_package_share_directory('f1tenth_gym_ros'),
+        'config',
+        'sim.yaml'
+        )
+    config_dict = yaml.safe_load(open(config, 'r'))
+    has_opp = config_dict['bridge']['ros__parameters']['num_agent'] > 1
+    teleop = config_dict['bridge']['ros__parameters']['kb_teleop']
+
+    bridge_node = Node(
+        package='f1tenth_gym_ros',
+        executable='gym_bridge',
+        name='bridge',
+        parameters=[config]
+    )
+
+    ego_robot_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='ego_robot_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', os.path.join(get_package_share_directory('f1tenth_gym_ros'), 'launch', 'ego_racecar.xacro')])}],
+        remappings=[('/robot_description', 'ego_robot_description')]
+    )
+    opp_robot_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='opp_robot_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', os.path.join(get_package_share_directory('f1tenth_gym_ros'), 'launch', 'opp_racecar.xacro')])}],
+        remappings=[('/robot_description', 'opp_robot_description')]
+    )
+
+    # finalize
+    ld.add_action(bridge_node)
+    ld.add_action(ego_robot_publisher)
+    if has_opp:
+        ld.add_action(opp_robot_publisher)
+
+    return ld
