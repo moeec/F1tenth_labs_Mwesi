bda11c0b7d88ab8ef99f6f58df94c29f9d8da6a5
Mwesi <mwesi@yahoo.com>
2025-02-12 17:07:56 -0500

    Update pure_pursuit_node.cpp

--- lab6_ws/pure_pursuit/src/pure_pursuit_node.cpp
+++ lab6_ws/pure_pursuit/src/pure_pursuit_node.cpp
@@ -88,8 +88,8 @@ private:
         RCLCPP_INFO(this->get_logger(), "Received odometry data");
         // Process the Odometry message
 
-        auto position_odom = msg->pose.pose.position;
-        auto orientation_odom = msg->pose.pose.orientation;
+        auto position_odom = msg->pose.pose.position; // Directly access position
+        auto orientation_odom = msg->pose.pose.orientation; // Directly access orientation
         
         RCLCPP_INFO(this->get_logger(),"Current Position is: x=%.2f, y=%.2f, z=%.2f", position_odom.x, position_odom.y, position_odom.z);
         RCLCPP_INFO(this->get_logger(),"Orientation (qx=%.2f, qy=%.2f, qz=%.2f, qw=%.2f)", orientation_odom.x, orientation_odom.y, orientation_odom.z, orientation_odom.w);
@@ -101,8 +101,8 @@ private:
     {
         RCLCPP_INFO(this->get_logger(), "Received pose data");
 
-        auto position_ps = pose_msg->pose.pose.position;
-        auto orientation_ps = pose_msg->pose.pose.orientation;
+        auto position_ps = pose_msg->pose.position;   // Directly access position
+        auto orientation_ps = pose_msg->pose.orientation;  // Directly access orientation
 
         RCLCPP_INFO(this->get_logger(),"Current Position is: x=%.2f, y=%.2f, z=%.2f", position_ps.x, position_ps.y, position_ps.z);
         RCLCPP_INFO(this->get_logger(),"Orientation (qx=%.2f, qy=%.2f, qz=%.2f, qw=%.2f)", orientation_ps.x, orientation_ps.y, orientation_ps.z, orientation_ps.w);
