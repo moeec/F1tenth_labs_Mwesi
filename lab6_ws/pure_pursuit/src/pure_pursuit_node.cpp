81c0a0f82d70163a47a3842006b23a55cee69bef
Mwesi <mwesi@yahoo.com>
2025-02-12 19:59:05 -0500

    Add files via upload

--- lab6_ws/pure_pursuit/src/pure_pursuit_node.cpp
+++ lab6_ws/pure_pursuit/src/pure_pursuit_node.cpp
@@ -15,7 +15,7 @@ public:
     {
         // Create a subscription to the Odometry message
         sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
-            "/odom", 1000, std::bind(&PurePursuit::callback, this, std::placeholders::_1));
+            "/ego_racecar/odom", 1000, std::bind(&PurePursuit::callback, this, std::placeholders::_1));
         
         
         marker_pub_ = this->create_publisher<visualization_msgs::msg::Marker>("waypoint_marker", 1000);
@@ -88,8 +88,8 @@ private:
         RCLCPP_INFO(this->get_logger(), "Received odometry data");
         // Process the Odometry message
 
-        auto position_odom = msg->pose.pose.position; // Directly access position
-        auto orientation_odom = msg->pose.pose.orientation; // Directly access orientation
+        auto position_odom = msg->pose.pose.position;
+        auto orientation_odom = msg->pose.pose.orientation;
         
         RCLCPP_INFO(this->get_logger(),"Current Position is: x=%.2f, y=%.2f, z=%.2f", position_odom.x, position_odom.y, position_odom.z);
         RCLCPP_INFO(this->get_logger(),"Orientation (qx=%.2f, qy=%.2f, qz=%.2f, qw=%.2f)", orientation_odom.x, orientation_odom.y, orientation_odom.z, orientation_odom.w);
@@ -101,8 +101,8 @@ private:
     {
         RCLCPP_INFO(this->get_logger(), "Received pose data");
 
-        auto position_ps = pose_msg->pose.position;   // Directly access position
-        auto orientation_ps = pose_msg->pose.orientation;  // Directly access orientation
+        auto position_ps = pose_msg->pose.position;
+        auto orientation_ps = pose_msg->pose.orientation;
 
         RCLCPP_INFO(this->get_logger(),"Current Position is: x=%.2f, y=%.2f, z=%.2f", position_ps.x, position_ps.y, position_ps.z);
         RCLCPP_INFO(this->get_logger(),"Orientation (qx=%.2f, qy=%.2f, qz=%.2f, qw=%.2f)", orientation_ps.x, orientation_ps.y, orientation_ps.z, orientation_ps.w);
